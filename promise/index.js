dde76c77b857962f60d329317fff376a3a54b314
rjwx60 <614054722@qq.com>
2019-04-26 10:14:06 +0800

    init

---
+++ promise/index.js
@@ -0,0 +1,188 @@
+class APromise{
+  constructor(executor){
+    this.state = 'pending';
+    this.value = undefined;
+    this.reason = undefined;
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks = [];
+    let resolve = value => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = value;
+        this.onResolvedCallbacks.forEach(fn=>fn());
+      }
+    };
+    let reject = reason => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach(fn=>fn());
+      }
+    };
+    try{
+      executor(resolve, reject);
+    } catch (err) {
+      reject(err);
+    }
+  }
+  then(onFulfilled, onRejected) {
+    // 限制 onFulfilled & onRejected 为函数
+    // 若非函数则直接返回 value
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    // 若非函数则直接抛出错误
+    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
+
+    let promise2 = new APromise((resolve, reject) => {
+      if (this.state === 'fulfilled') {
+        // 异步
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      };
+      if (this.state === 'rejected') {
+        // 异步
+        setTimeout(() => {
+          // 如果报错
+          try {
+            let x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      };
+      if (this.state === 'pending') {
+        this.onResolvedCallbacks.push(() => {
+          // 异步
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+        this.onRejectedCallbacks.push(() => {
+          // 异步
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0)
+        });
+      };
+    });
+    // 返回promise，完成链式
+    return promise2;
+  }
+}
+
+
+
+function resolvePromise(promise2, x, resolve, reject){
+  // 防止循环引用
+  if(x === promise2){
+    return reject(new TypeError('Chaining cycle detected for promise'));
+  }
+
+  // 防止多次调用
+  let called;
+
+  //  对象或函数处理
+  if (x != null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      // A+规定，声明then = x 的 then 方法
+      let then = x.then;
+      // 如果 then 是函数，就默认是promise了(鸭子类型)
+      if (typeof then === 'function') { 
+        // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
+        then.call(x, y => {
+          // 成功和失败只能调用一个
+          if (called) return;
+          called = true;
+          // resolve的结果依旧是promise 那就继续解析
+          resolvePromise(promise2, y, resolve, reject);
+        }, err => {
+          // 成功和失败只能调用一个
+          if (called) return;
+          called = true;
+          reject(err);// 失败了就失败了
+        })
+      } else {
+        resolve(x); // 直接成功即可
+      }
+    } catch (e) {
+      // 也属于失败
+      if (called) return;
+      called = true;
+      // 取then出错了那就不要在继续执行了
+      reject(e); 
+    }
+  } else {
+    // 若为基本类型值则直接 resolve
+    resolve(x);
+  }
+}
+
+
+// resolve方法
+APromise.resolve = function(val){
+  return new APromise((resolve,reject)=>{
+    resolve(val)
+  });
+}
+// reject方法
+APromise.reject = function(val){
+  return new APromise((resolve,reject)=>{
+    reject(val)
+  });
+}
+// race方法 
+APromise.race = function(promises){
+  return new APromise((resolve,reject)=>{
+    for(let i = 0; i < promises.length; i++){
+      promises[i].then(resolve,reject)
+    };
+  })
+}
+// all方法(获取所有的promise，都执行then，把结果放到数组，一起返回)
+APromise.all = function(promises){
+  let arr = [];
+  let i = 0;
+  function processData(index,data){
+    arr[index] = data;
+    i++;
+    if(i == promises.length){
+      resolve(arr);
+    };
+  };
+  return new APromise((resolve,reject)=>{
+    for(let i=0;i<promises.length;i++){
+      promises[i].then(data=>{
+        processData(i,data);
+      },reject);
+    };
+  });
+}
+
+
+APromise.deferred = function() {
+  let defer = {};
+  defer.promise = new APromise((resolve, reject) => {
+    defer.resolve = resolve;
+    defer.reject = reject;
+  });
+  return defer;
+};
+
+module.exports = APromise;
+
---
+++ promise/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+  // // 成功
+    // let resolve = () => { };
+    // // 失败
+    // let reject = () => { };
+    // // 立即执行
+    // executor(resolve, reject);
